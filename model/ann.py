5ab199b291313f7a982ecd89138ed04f0838aec7
Chetna Khanna <chetnakhanna16@gmail.com>
2020-08-26 09:41:05 -0700

    Updated ANN and LSTM models

--- model/ann.py
+++ model/ann.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 from keras.optimizers import SGD
 from keras.models import Sequential
-from keras.layers import Dropout, Dense, LSTM, Masking, Embedding, SpatialDropout1D
+from keras.layers import Dropout, Dense
 from keras.models import load_model
 import h5py
 import logging
 import pathlib
 
+
+#class to load the ANN model
 class AnnModel:
 
 	model = None
--- model/lstm.py
+++ model/lstm.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
-from keras.optimizers import SGD
 from keras.models import Sequential
-from keras.layers import Dropout, Dense, LSTM, Masking, Embedding, SpatialDropout1D
+from keras.layers import Dropout, Dense, LSTM, Embedding, SpatialDropout1D
 from keras.models import load_model
 import h5py
 import logging
 import pathlib
 
+
+#class to load the LSTM model
 class LstmModel:
 
 	model = None
